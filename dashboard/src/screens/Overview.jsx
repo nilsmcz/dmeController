869b03b02a333c766ddf9be32fdc25de7bf8f910
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2024-07-25 19:52:03 +0200

    fetch testAlarms from db

--- dashboard/src/screens/Overview.jsx
+++ dashboard/src/screens/Overview.jsx
@@ -1,44 +1,46 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
-//sideEffects:
-import { getTestAlarms } from '../sideEffects/sideEffects'
+// sideEffects:
+import { getTestAlarms } from '../sideEffects/sideEffects';
 
 export default function Overview() {
 
-    //states:
-    const [alarms, setAlarms] = useState([])
+    // states:
+    const [alarms, setAlarms] = useState([]);
 
     useEffect(() => {
-        const testAlarms = async () => {
-            const alarms = await getTestAlarms()
-            console.log(alarms)
+        async function fetchAlarms() {
+            const fetchedAlarms = await getTestAlarms();
+            setAlarms(fetchedAlarms || []);
         }
-        testAlarms()
-    }, [])
+        fetchAlarms();
+    }, []);
 
+    const daysOfWeek = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
 
-  return (
-    <div style={{display:"flex", justifyContent:"start", flexDirection:"column", alignItems:"start", padding:"15px", gap:"5px"}}>
-
-        <div>
-            <div style={{fontSize:"13px", fontWeight:"500", color:"grey"}}>Jeden Freitag um</div>
-
-            <div style={{display:"flex", flexDirection:"row", gap:"30px"}}>
-                <div style={{fontSize:"19px", fontWeight:"600"}}>18:00 Uhr</div>
-                <div style={{fontSize:"19px", fontWeight:"600"}}>+- 5 Minuten</div>
-            </div>
+    return (
+        <div style={{ display: "flex", justifyContent: "start", flexDirection: "column", alignItems: "start", padding: "15px", gap: "5px" }}>
+            {alarms.map((alarm, index) => {
+                if (!alarm) return null; // Skip null values
+                return (
+                    <div key={index}>
+                        <div style={{ fontSize: "13px", fontWeight: "500", color: "grey" }}>
+                            Jeden {daysOfWeek[alarm.day]} um
+                        </div>
+                        <div style={{ display: "flex", flexDirection: "row", gap: "30px" }}>
+                            <div style={{ fontSize: "19px", fontWeight: "600" }}>
+                                {`${alarm.hour.toString().padStart(2, '0')}:${alarm.minute.toString().padStart(2, '0')} Uhr`}
+                            </div>
+                            <div style={{ fontSize: "19px", fontWeight: "600" }}>
+                                {`- ${alarm.range.before} Minuten`}
+                            </div>
+                            <div style={{ fontSize: "19px", fontWeight: "600" }}>
+                                {`+ ${alarm.range.after} Minuten`}
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
         </div>
-
-        <div>
-            <div style={{fontSize:"13px", fontWeight:"500", color:"grey"}}>Jeden Samstag um</div>
-
-            <div style={{display:"flex", flexDirection:"row", gap:"30px"}}>
-                <div style={{fontSize:"19px", fontWeight:"600"}}>12:00 Uhr</div>
-                <div style={{fontSize:"19px", fontWeight:"600"}}>+- 15 Minuten</div>
-            </div>
-        </div>
-    </div>
-  )
+    );
 }
