51f5f2161ac20803d3c33f174fa3e2d4519ad258
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2024-07-25 15:35:44 +0200

    tabs style

--- dashboard/src/navigation/Tabs.jsx
+++ dashboard/src/navigation/Tabs.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
 import { Tabs, rem } from '@mantine/core';
-import History from '../screens/History';
-
 import { useState } from 'react';
+
+//screens:
+import History from '../screens/History';
 import Overview from '../screens/Overview';
 import Settings from '../screens/Settings';
+
+
+//icons:
+import settings from '../assets/icons/settings.svg';
+import history from '../assets/icons/history.svg';
+import home from '../assets/icons/home.svg';
 
 export default function TabsLayout() {
 
     return (
         <Tabs defaultValue="overview" color="rgba(13, 13, 13, 1)">
             <Tabs.List grow justify="space-between">
-                <Tabs.Tab value="history" style={{ flex: 1, textAlign: 'center' }}>
+                <Tabs.Tab value="history" style={{ flex: 1, textAlign: 'center' }} leftSection={<img src={history} alt="history icon" />}>
                     Historie
                 </Tabs.Tab>
-                <Tabs.Tab value="overview" style={{ flex: 1, textAlign: 'center' }}>
+                <Tabs.Tab value="overview" style={{ flex: 1, textAlign: 'center' }} leftSection={<img src={home} alt="home icon" />}>
                     Übersicht
                 </Tabs.Tab>
-                <Tabs.Tab value="settings" style={{ flex: 1, textAlign: 'center' }}>
+                <Tabs.Tab value="settings" style={{ flex: 1, textAlign: 'center' }} leftSection={<img src={settings} alt="settings icon" />}>
                     Einstellungen
                 </Tabs.Tab>
             </Tabs.List>
