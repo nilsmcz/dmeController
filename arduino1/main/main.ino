f71d26b0e1037622b5035c68fe61edb5a0fe2111
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2024-07-27 21:09:11 +0200

    ardunio uno relay contact

---
+++ arduino1/main/main.ino
@@ -0,0 +1,15 @@
+const int relayContact = A0;
+int relayContactValue = 0;
+
+void setup() {
+  Serial.begin(4800);
+  pinMode(relayContact, INPUT);
+}
+
+void loop() {
+  relayContactValue = analogRead(relayContact);
+  if(relayContactValue > 1){
+    Serial.println("SIGNAL");
+  }
+  delay(100);
+}
